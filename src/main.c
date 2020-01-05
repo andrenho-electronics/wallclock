95b47c5c65ea2c1ec238467be5071a353e0de18e
André Wagner <andre.nho@gmail.com>
2020-01-05 00:18:26 +0000

    .

--- src/main.c
+++ src/main.c
@@ -1,32 +1,62 @@
 #define F_CPU 8000000UL
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-uint8_t hour = 0,                   // current hour
-        minute = 0;                 // current minute
+volatile uint8_t hour = 9,                         // current hour
+                 minute = 34;                       // current minute
 
-typedef enum { SHOW_TIME } State;   // current clock state
-uint8_t current_digit = 0;          // current digit being displayed (changes at 1000 Hz)
+typedef enum { SHOW_TIME } State;         // current clock state
+volatile State  state = SHOW_TIME;
+
+volatile uint8_t current_digit = 0;                // current digit being displayed (changes at 1000 Hz)
+volatile uint8_t digit[4] = { 0, 0, 0, 0 };        // current number for each one of the 4 digits
 
 typedef struct {
-    unsigned int update_clock : 1;
+    bool update_clock : 1;
 } Events;
+volatile Events events = { 0 };
 
-// globals
-State  state = SHOW_TIME;
-Events events = { .update_clock = 0 };
-int    timer1_counter = 0;
+volatile unsigned long timer1_counter = 0;
+volatile bool show_point = false;
+
+uint8_t images[] = {
+    //CG.DEBFA
+    0b10011111,  // 0
+    0b10000100,  // 1
+    0b01011101,  // 2
+    0b11010101,  // 3
+    0b11000110,  // 4
+    0b11010011,  // 5
+    0b11011011,  // 6
+    0b10000101,  // 7
+    0b11011111,  // 8
+    0b11010111,  // 9
+
+    0b10111111,  // 0.
+    0b10100100,  // 1.
+    0b01111101,  // 2.
+    0b11110101,  // 3.
+    0b11100110,  // 4.
+    0b11110011,  // 5.
+    0b11111011,  // 6.
+    0b10100101,  // 7.
+    0b11111111,  // 8.
+    0b11110111,  // 9.
+
+    0b00000000,  // empty (20)
+};
 
 static void iosetup()
 {
     // setup ports
     DDRB |= _BV(DDB1) | _BV(DDB2) | _BV(DDB3) | _BV(DDB4);  // PORTD - choose digit
     DDRD = 0xFF;  // PORTD - digit image
-    PORTD = 0xF0;
+    PORTD = 0x00;
 
     cli();
     // setup timer 0 for 7-seg digit rotation (1000 Hz)
@@ -54,6 +84,10 @@ static void update_clock() {
 }
 
 static void set_digits() {
+    digit[0] = (hour / 10);
+    digit[1] = (hour % 10);
+    digit[2] = (minute / 10);
+    digit[3] = (minute % 10);
 }
 
 // update 7seg digit - runs at 1000 Hz
@@ -64,21 +98,31 @@ ISR(TIMER0_COMPA_vect) {
     pb &= mask;
     pb |= (~mask & (1 << (current_digit + 1)));
 
+    // prepare digit image
+    uint8_t tdigit = digit[current_digit];
+    if (current_digit == 1 && show_point)
+        tdigit += 10;
+    if (current_digit == 0 && tdigit == 0)
+        tdigit = 20;
+
+    // draw digit
+    PORTB = pb;
+    PORTD = ~images[tdigit];
+
     // advance to next digit
     if (current_digit == 3)
         current_digit = 0;
     else
         ++current_digit;
-
-    // draw digit
-    PORTB = pb;
 }
 
 // update events - runs at 50 Hz
 ISR(TIMER1_COMPA_vect) {
     if (timer1_counter % 10 == 0)
-        events.update_clock = 1;
-    // TODO - events
+        events.update_clock = true;
+    if (timer1_counter % 40 == 0)
+        show_point = !show_point;
+    ++timer1_counter;
 }
 
 int main() {
@@ -87,10 +131,9 @@ int main() {
         if (events.update_clock) {
             update_clock();
             set_digits();
+            events.update_clock = false;
         }
     }
-
-    events = (Events) { 0 };
 }
 
 // vim: st=4:sts=4:sw=4:expandtab
